890b219932f152da11b54801177200ed439bfa14
Dayanne Mendes <daymendesdev@gmail.com>
2024-09-26 10:17:43 -0300

    inclusão de div e animação
    incluindo a div container-apresentacao

---
+++ assets/js/script.js
@@ -0,0 +1,4 @@
+window.addEventListener('load', () => {
+    const containerApresentacao = document.querySelector('.container-apresentacao');
+    containerApresentacao.classList.add('animated');
+});
