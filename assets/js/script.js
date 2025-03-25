d502c78003f8d38f44977b80f0799a957c38ad96
Dayanne Mendes <daymendesdev@gmail.com>
2025-03-25 15:35:48 -0300

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,3 +2,9 @@ window.addEventListener('load', () => {
     const containerApresentacao = document.querySelector('.container-apresentacao');
     containerApresentacao.classList.add('animated');
 });
+
+document.querySelectorAll('.btn').forEach(btn => {
+  btn.addEventListener('click', (event) => {
+    alert('Você será redirecionado para o projeto!');
+  });
+});
