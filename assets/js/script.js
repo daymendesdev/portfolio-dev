b7789252a894be7bab7ea59445750042f1ac5fd7
Dayanne Mendes <daymendesdev@gmail.com>
2025-06-25 22:26:16 -0300

    feat: alteração visual, mas sem responsividade ainda

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,9 +2,3 @@ window.addEventListener('load', () => {
     const containerApresentacao = document.querySelector('.container-apresentacao');
     containerApresentacao.classList.add('animated');
 });
-
-document.querySelectorAll('.btn').forEach(btn => {
-  btn.addEventListener('click', (event) => {
-    alert('Você será redirecionado para o projeto!');
-  });
-});
